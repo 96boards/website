936fce3a25859e5a8d5587807bb5b6652c98ccf7
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-03-06 11:22:04 +0000

    Added the home carousels to the home-custom.js file

--- assets/js/app/home-custom.js
+++ assets/js/app/home-custom.js
@@ -1,19 +1,68 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    lazyLoad: true,
-    autoplay:true,
-    autoplayTimeout:3000,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
+$(document).ready(function() {
+
+  var homeCarousel = $(".home-carousel");
+  var specificationCarousel = $(".specification-carousel");
+  var blogCarousel = $(".blog-carousel");
+
+  homeCarousel.owlCarousel({
+      loop:false,
+      margin:10,
+      nav:false,
+      dots: true,
+      lazyLoad: true,
+      margin: 25,
+      autoplay:false,
+      responsiveClass: true,
+      navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+      responsive:{
+          0:{
+              items:1,
+              dots: true,
+              nav: false
+          }
+      }
+  });
+  
+  specificationCarousel.owlCarousel({
+      loop:false,
+      margin:10,
+      nav:false,
+      dots: true,
+      lazyLoad: true,
+      margin: 25,
+      autoplay:false,
+      responsiveClass: true,
+      navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+      responsive:{
+          0:{
+              items:1,
+              dots: true,
+              nav: false
+          }
+      }
+  });
+  blogCarousel.owlCarousel({
+      loop:false,
+      margin:10,
+      nav:false,
+      dots: true,
+      lazyLoad: true,
+      margin: 25,
+      autoplay:false,
+      responsiveClass: true,
+      navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+      responsive:{
+          0:{
+              items:1,
+              dots: true,
+              nav: false
+          },
+          676:{
+              items:2,
+              dots: true,
+              nav: false
+          }
+      }
+  });
+
 });
