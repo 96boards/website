d57c1d701a308323e60f8edb612390b7a368522c
Kyle Kirkby <kyle.kirkby@linaro.org>
2018-04-30 10:56:13 +0100

    Updated projects.js and blog submission form.

--- assets/js/app/projects.js
+++ assets/js/app/projects.js
@@ -18,23 +18,23 @@ $('.owl-carousel').owlCarousel({
     }
 });
 
-var mixer = mixitup('.mixitup-container', {
-    selectors: {
-        target: '.project-item',
-        control: '[data-mixitup-control]'
-    },
-    animation: {
-        duration: 300
-    }
-});
 
-
-$filterSelect = $('.filter-select');
-$container = $('.mixitup-container');
-
-$filterSelect.on('change', function(){
-    mixer.filter(this.value);
-});
+if($('.mixitup-container').length) {
+    var mixer = mixitup('.mixitup-container', {
+        selectors: {
+            target: '.project-item',
+            control: '[data-mixitup-control]'
+        },
+        animation: {
+            duration: 300
+        }
+    });
+    $filterSelect = $('.filter-select');
+    $container = $('.mixitup-container');
+    $filterSelect.on('change', function(){
+        mixer.filter(this.value);
+    });
+}
 
 
 
