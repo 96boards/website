0d9f88ce296413bf58e1865e6508ffad59773d2d
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-01-14 10:15:57 +0000

    Added a compare products js package

---
+++ assets/js/package-products-compare.js
@@ -0,0 +1,10 @@
+//= require vendor/jquery
+//= require vendor/bootstrap
+//= require vendor/cookieconsent
+//= require vendor/lazysizes
+//= require vendor/featherlight
+
+//= require app/main
+//= requrie app/search
+//= require app/products-compare
+//= require app/custom
