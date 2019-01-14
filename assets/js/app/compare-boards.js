4134cd7d26fb250a7d9eef8def76ef8600130262
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-01-14 17:13:38 +0000

    Updated the compare-boards.js to remove boards when the x icon is clicked in the table headings

--- assets/js/app/compare-boards.js
+++ assets/js/app/compare-boards.js
@@ -1,9 +1,19 @@
+
+function isOverflown(element) {
+    return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
+}
+
 $(window).on('load', function () {
+    if (isOverflown(document.getElementById("compare-table"))) {
+        $(".double-scroll").doubleScroll();
+    }
     // Enabled the multiselect plugin
     $("#compare-96boards-select").multiselect({
         nonSelectedText: 'Select 96Boards',
         includeSelectAllOption: true,
+        enableFiltering: true,
         nSelectedText: ' boards selected',
+        allSelectedText: 'All 96Boards',
         selectAllText: 'Compare all 96Boards',
         onSelectAll: function () {
             $("[data-board]").each(function(){
@@ -17,6 +27,9 @@ $(window).on('load', function () {
         },
         enableCollapsibleOptGroups: true,
         onChange: function (option, checked, select) {
+            if (isOverflown(document.getElementById("compare-table"))) {
+                $(".double-scroll").doubleScroll();
+            }
             if(checked == true){
                 $("[data-board='" + $(option).val() + "'").css("display", "table-cell");
             }
@@ -38,41 +51,46 @@ $(window).on('load', function () {
         }
         return vars;
     }
-    // Get all the Url Vars
-    var boardParams = getUrlVars()["boards"].split(",");
-    var validBoards = [];
-    // Get vars from table
-    $("th.board").each(function(){
-        validBoards.push($(this).data("board"));
+    // Pop board off the table with x icon in th
+    $("i.fa.fa-times").on("click",function(){
+        var boardSelector = "[data-board='" + $(this).parent().data("board") + "']";
+        $(boardSelector).css("display", "none");
+        $("#compare-96boards-select").multiselect('deselect', $(this).parent().data("board"));
     });
-    $.each(boardParams, function(index, value){
-        console.log(value);
-        if($.inArray(value, validBoards)){
-            // Select the relevant options in the Boostrap Multiselect
-            $("#compare-96boards-select").multiselect('select', value);
-            // Get the data-board selector
-            var boardSelector = "[data-board='" + value + "']";
-            console.log(boardSelector);
-            // Toggle cells based on the board selector from display:none; to display:table-cell;
-            $(boardSelector).css("display","table-cell");
-        }
-        else{
-            console.log(value + "is not in the valid Boards array");
-            console.log(validBoards);
-        }
-    });
+    // Check to see if there are in boards in GET params
+    if (typeof getUrlVars()["boards"] === 'undefined'){
+        // If not then select all boards
+        $("#compare-96boards-select").multiselect('selectAll', true);
+    }
+    else {
+        // parse the boards param and display boards
+        var boardParams = getUrlVars()["boards"].split(",");
+        var validBoards = [];
+        // Get vars from table
+        $("th.board").each(function () {
+            validBoards.push($(this).data("board"));
+        });
+        $.each(boardParams, function (index, value) {
+            console.log(value);
+            if ($.inArray(value, validBoards) > -1) {
+                // Select the relevant options in the Boostrap Multiselect
+                $("#compare-96boards-select").multiselect('select', value);
+                // Get the data-board selector
+                var boardSelector = "[data-board='" + value + "']";
+                console.log(boardSelector);
+                // Toggle cells based on the board selector from display:none; to display:table-cell;
+                $(boardSelector).css("display", "table-cell");
+            }
+            else {
+                console.log(value + " is not in the valid Boards array");
+                console.log(validBoards);
+            }
+        });
+    }
 });
-
-function isOverflown(element) {
-    return element.scrollHeight > element.clientHeight || element.scrollWidth > element.clientWidth;
-}
 
 $(window).on("scroll", function(){
     if(isOverflown(document.getElementById("compare-table"))){
         $(".double-scroll").doubleScroll();
-        console.log("overflow!!!");
-    }
-    else{
-        console.log("not overflown");
     }
 });
