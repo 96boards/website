7a7d58e6bf9a27e2dc00f3b8246ba4d9f0641b2e
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-01-16 16:51:42 +0000

    Added check for the .owl-carousel element

--- assets/js/app/products.js
+++ assets/js/app/products.js
@@ -1,23 +1,25 @@
 $(window).on('load', function () {
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: false,
-        lazyLoad: true,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 1
+    if($(".owl-carousel").length){
+        $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: false,
+            lazyLoad: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
             }
-        }
-    });
+        });
+    }
     if ($('.mixitup-container').length) {
         var mixer = mixitup('.mixitup-container', {
             selectors: {
