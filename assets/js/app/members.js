c6fa93f8713fc881901b0c42ecaf75d110a8d2c1
Kyle Kirkby <kyle.kirkby@linaro.org>
2019-03-11 12:27:12 +0000

    Refactored the members.js file

--- assets/js/app/members.js
+++ assets/js/app/members.js
@@ -13,24 +13,35 @@ $(document).ready(function(){
     });
 });
 // This functions adds the members to a the selector element
-function addMembers(members, selector){
+function addMembers(membersData, membersKey, selector){
+
+    // Required members data
+    var requiredMembers = [];
+    // Get required members
+    for(i=0;i<membersData.length;i++){
+        if(membersData[i]["id"] === membersKey){
+            requiredMembers = membersData[i];
+        }
+    }
     var elements = "";
     // Loop through the members
-    for(i=0;i<members.length;i++){
+    for(i=0;i<requiredMembers["members"].length;i++){
         // Create new element based on current member index in loop
         var memberElement = '<div class="col-md-3 col-sm-4 col-xs-6">';
-        memberElement += '<a href="' + members[i].url + '" target="_blank">';
-        memberElement += '<img data-src="' + members[i].image + '" alt="' + members[i].name + '"';
-        memberELemetn += 'src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" class="img-responsive members-img center-block lazyload"/>';
+        memberElement += '<a href="' + requiredMembers["members"][i].url + '" target="_blank">';
+        memberElement += '<img data-src="' + requiredMembers["members"][i].image + '" alt="' + requiredMembers["members"][i].name + '"';
+        memberElement += 'src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" class="img-responsive members-img center-block lazyload"/>';
         memberElement += '</a></div>';
         // Append to elements
         elements += memberElement;
     }
-    $(selector).html(elements)
+    $(selector).html(elements);
+    console.log(membersKey +": " + elements);
 }
 // Wait for AJAX request to stop
 $(document).ajaxStop(function () {
-    addMembers(membersJSONData["boards-sc"], "#boards-sc");
-    addMembers(membersJSONData["boards-mezzanine"], "#boards-mezzanine");
-    addMembers(membersJSONData["boards-mp"], "#boards-mp");
+    console.log(membersJSONData);
+    addMembers(membersJSONData, "boards-sc", "#boards-sc");
+    addMembers(membersJSONData, "boards-mezzanine", "#boards-mezzanine");
+    addMembers(membersJSONData, "boards-mp", "#boards-mp");
 });
