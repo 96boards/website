07ff44400d197a48d96d6044182f6575dfc89f1e
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-04-30 13:49:04 +0100

    Updated twentytwenty handles

--- assets/js/app/custom.js
+++ assets/js/app/custom.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     $("#beforeAndAfterContainer").twentytwenty({
       default_offset_pct: 0.5, // How much of the before image is visible when the page loads
       //   orientation: "vertical", // Orientation of the before and after images ('horizontal' or 'vertical')
-      before_label: "IoT Box", // Set a custom before label
+      before_label: "Stinger96", // Set a custom before label
       after_label: "Avenger96", // Set a custom after label
       //   no_overlay: true, //Do not show the overlay with before and after
       //   move_slider_on_hover: true, // Move slider on mouse hover?
